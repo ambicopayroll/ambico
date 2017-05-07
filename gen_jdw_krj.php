81b50f6c730fc01bf297f10db3a8d93cbc138f53
fujitsu <ambicopayroll@gmail.com>
2017-05-07 11:16:41 +0700

    upd

--- gen_jdw_krj.php
+++ gen_jdw_krj.php
@@ -1,22 +1,22 @@
-<?php include_once "phpfn13.php" ?>
+<?php //include_once "phpfn13.php" ?>
 <?php
-$conn =& DbHelper(); 
-//include "adodb5/adodb.inc.php";
+//$conn =& DbHelper(); 
+include "adodb5/adodb.inc.php";
 
-//$conn = ADONewConnection('mysql');
+$conn = ADONewConnection('mysql');
 //$conn->Connect('localhost','root','admin','fin_pro');
 
-/*if ($_SERVER["HTTP_HOST"] == "localhost" ) { // testing on local PC
+if ($_SERVER["HTTP_HOST"] == "localhost" ) { // testing on local PC
 	$conn->Connect('localhost','root','admin','fin_pro');
 } elseif ($_SERVER["HTTP_HOST"] == "ambico.nma-indonesia.com") { // setting koneksi database untuk komputer server
 	$conn->Connect('mysql.idhostinger.com','u945388674_ambic','M457r1P 81','u945388674_ambic');
-}*/
+}
 
 $rs = $conn->Execute('select * from t_jdw_krj_peg order by pegawai_id, tgl1');
-$msql = 'select * from t_jdw_krj_peg order by pegawai_id, tgl1'; echo $msql; exit;
+//$msql = 'select * from t_jdw_krj_peg order by pegawai_id, tgl1'; echo $msql; exit;
 
 while (!$rs->EOF) {
-	echo "1"; exit;
+	//echo "1"; exit;
 	$mpegawai_id = $rs->fields["pegawai_id"];
 	$mtanggal_hari_ini = date("Y-m-d");
 	//$msql = "delete from t_jdw_krj_def where pegawai_id = ".$mpegawai_id." and tgl > '".$mtanggal_hari_ini."'"; //echo $msql; exit;
