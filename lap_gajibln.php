72810acc8da167ef53c4f4b85f74e01e9353dcc6
pc_it_storage <ambicopayroll@gmail.com>
2017-05-08 14:42:09 +0700

    1

--- lap_gajibln.php
+++ lap_gajibln.php
@@ -1,12 +1,12 @@
 <?php
-if ($_SERVER["HTTP_HOST"] == "localhost" or $_SERVER["HTTP_HOST"] == "36.80.56.64") {
-	include_once "phpfn13.php";
-	$conn =& DbHelper();
-}
-elseif ($_SERVER["HTTP_HOST"] == "ambico.nma-indonesia.com") {
+if ($_SERVER["HTTP_HOST"] == "ambico.nma-indonesia.com") {
 	include "adodb5/adodb.inc.php";
 	$conn = ADONewConnection('mysql');
 	$conn->Connect('mysql.idhostinger.com','u945388674_ambi2','M457r1P 81','u945388674_ambi2');
+}
+else {
+	include_once "phpfn13.php";
+	$conn =& DbHelper();
 }
 
 $msql = "delete from t_gjbln";
